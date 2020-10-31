6140a6490baf58adf5835f23db5dec1dc1f3116a
JakubAndrysek <jakub.andrysek03@gmail.com>
2020-10-31 22:09:36 +0100

    Add git-updater

---
+++ git-updater.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e # Stop on error
+
+while :
+do
+    date
+    git fetch
+    git reset --hard origin/master
+    git clean -f
+
+    # Restart your services here
+
+    while :
+    do
+        sleep 3600
+        git fetch
+        if git status | grep 'behind'
+        then
+            break
+        fi
+    done
+done  
