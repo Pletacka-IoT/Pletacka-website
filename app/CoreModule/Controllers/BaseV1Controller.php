cb90a77f06ea3df48eec6eef7d7fcf9d98235331
JakubAndrysek <jakub.andrysek03@gmail.com>
2020-12-29 01:22:46 +0100

    Finished update

--- app/CoreModule/Controllers/BaseV1Controller.php
+++ app/CoreModule/Controllers/BaseV1Controller.php
@@ -2,11 +2,11 @@
 
 namespace App\CoreModule\Controllers;
 
-use Apitte\Core\Annotation\Controller\GroupPath;
+use Apitte\Core\Annotation\Controller\Path;
 
 /**
  * Main API v1 class - extend BaseController
- * @GroupPath("/v1")
+ * @Path("/v1")
  */
 abstract class BaseV1Controller extends BaseController
 {
